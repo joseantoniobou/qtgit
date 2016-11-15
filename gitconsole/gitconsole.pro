74ee5f4825985c1e6973932f9524ad1cb3d3d529
joseantoniobou <joseantoniobou@icloud.com>
2016-11-15 18:39:06 +0100

    Prueba con -am

--- gitconsole/gitconsole.pro
+++ gitconsole/gitconsole.pro
@@ -10,3 +10,5 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES += main.cpp
+
+HEADERS +=
--- gitconsole/main.cpp
+++ gitconsole/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     cout << "Hola git" << endl;
+    cout << "Ahora tambien hola TFS" << endl;
+    cout << "Voy a modificar mas cosas" << endl;
 
     return a.exec();
 }
